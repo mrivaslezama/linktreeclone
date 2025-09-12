fe057674e5fac8afb171035d5a630dc93ca3be2b
Miguel Rivas <mrivaslezma@gmail.com>
2025-09-12 01:11:58 -0300

    config ready app

---
+++ db/migrate/20250911151605_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :youtube
+      t.string :twitch
+      t.string :email
+      t.string :other
+
+      t.timestamps
+    end
+  end
+end
