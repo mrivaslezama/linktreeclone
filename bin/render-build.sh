c7752b64b59908203878d2acc1580dc1d10432b5
Miguel Rivas <mrivaslezma@gmail.com>
2025-09-12 23:59:52 -0300

    Fix Render deployment configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,13 @@
 
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bin/rails assets:precompile
 bin/rails assets:clean
 
+# Run database migrations
+# On Render, this will run after the database is available
 bin/rails db:migrate
