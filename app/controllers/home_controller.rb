33d195aceefdf69bc5a72002d19c640ee4e91fb8
Miguel Rivas <mrivaslezma@gmail.com>
2025-09-18 22:31:13 -0300

    warping my app try

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @profiles = Profile.all
   end
 end
---
+++ db/migrate/20250919011536_show_profile_fields.rb
@@ -0,0 +1,4 @@
+class ShowProfileFields < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
